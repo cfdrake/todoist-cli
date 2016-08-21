1c35d6bcb897a6bc432adeb6e8754c25f44a61cf
Colin Drake <colin.f.drake@gmail.com>
2016-08-21 16:53:19 -0500

    Run go fmt

--- cli/config.go
+++ cli/config.go
@@ -1,35 +1,35 @@
 package cli
 
 import (
-  "errors"
-  "os/user"
-  "path"
+	"errors"
+	"os/user"
+	"path"
 )
 
 import "github.com/vaughan0/go-ini"
 
 // Configuration file representation.
 type Config struct {
-  AuthToken string // Personal token used to authenticate with the REST API.
+	AuthToken string // Personal token used to authenticate with the REST API.
 }
 
 // Loads configuration for the given user.
 func loadConfiguration(user *user.User) (*Config, error) {
-  // Generate path to configuration file.
-  configFilePath := ".config/todoist-cli/config.ini"
-  path := path.Join(user.HomeDir, configFilePath)
+	// Generate path to configuration file.
+	configFilePath := ".config/todoist-cli/config.ini"
+	path := path.Join(user.HomeDir, configFilePath)
 
-  // Attempt to load the file into memory.
-  file, err := ini.LoadFile(path)
-  if err != nil {
-    return nil, errors.New("Could not load config file")
-  }
+	// Attempt to load the file into memory.
+	file, err := ini.LoadFile(path)
+	if err != nil {
+		return nil, errors.New("Could not load config file")
+	}
 
-  // Fetch the needed data.
-  token, ok := file.Get("auth", "token")
-  if !ok {
-    return nil, errors.New("Could not load authorization token")
-  }
+	// Fetch the needed data.
+	token, ok := file.Get("auth", "token")
+	if !ok {
+		return nil, errors.New("Could not load authorization token")
+	}
 
-  return &Config{AuthToken: token}, nil
+	return &Config{AuthToken: token}, nil
 }

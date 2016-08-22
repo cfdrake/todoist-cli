9e9251a7b393918d22f0b7838315b8ffbf40af5b
Colin Drake <colin.f.drake@gmail.com>
2016-08-22 08:19:12 -0500

    Simplify die() call

--- cli/util.go
+++ cli/util.go
@@ -7,8 +7,7 @@ import (
 
 // Prints a message and exits the program.
 func die(format string, a ...interface{}) {
-	fmt.Fprint(os.Stderr, "ERROR: ")
-	fmt.Fprintf(os.Stderr, format, a...)
-	fmt.Println()
+	formatStr := fmt.Sprintf("ERROR: %s\n", format)
+	fmt.Fprintf(os.Stderr, formatStr, a...)
 	os.Exit(1)
 }

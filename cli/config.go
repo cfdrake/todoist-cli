7a26f7dfafe747c8cbb521eab9c1b0befa0acdff
Colin Drake <colin.f.drake@gmail.com>
2016-08-21 21:31:11 -0500

    Move config population to a helper method

--- cli/config.go
+++ cli/config.go
@@ -13,23 +13,33 @@ type config struct {
 	userToken string // Personal token used to authenticate with the REST API.
 }
 
+// Populate a config instance from an INI.
+func (c *config) PopulateFromIni(file ini.File) {
+	token, ok := file.Get("auth", "token")
+	if ok {
+		c.userToken = token
+	}
+}
+
 // Loads configuration for the given user.
 func loadConfiguration(user *user.User) (*config, error) {
 	// Generate path to configuration file.
 	configFilePath := ".config/todoist-cli/config.ini"
 	path := path.Join(user.HomeDir, configFilePath)
 
-	// Attempt to load the file into memory.
+	// Load config from file.
 	file, err := ini.LoadFile(path)
 	if err != nil {
 		return nil, errors.New("Could not load config file")
 	}
 
-	// Fetch the needed data.
-	token, ok := file.Get("auth", "token")
-	if !ok {
+	c := new(config)
+	c.PopulateFromIni(file)
+
+	// Check for missing data.
+	if c.userToken == "" {
 		return nil, errors.New("Could not load authorization token")
 	}
 
-	return &config{userToken: token}, nil
+	return c, nil
 }
